db9cb0760caff3a0d59c9a4b92941c306ce124a1
Hance08 <hancechin08@gmail.com>
2023-06-01 21:40:03 +0800

    Update music_commands.py
    import packages
    setting ytdl_format_options
    setting ffmeg_options

---
+++ cogs/music_commands.py
@@ -0,0 +1,25 @@
+import discord
+import asyncio
+import yt_dlp as youtube_dl
+from discord import app_commands
+from discord import FFmpegPCMAudio
+from discord.ext import commands
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options' : '-vn'
+}
