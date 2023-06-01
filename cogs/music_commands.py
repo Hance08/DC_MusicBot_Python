ac58d4846d2f91bfde7a4de96f52171d2716e652
Hance08 <hancechin08@gmail.com>
2023-06-01 21:53:07 +0800

    Update music_commands.py
    add class YTDSource

--- cogs/music_commands.py
+++ cogs/music_commands.py
@@ -23,3 +23,23 @@ ytdl_format_options = {
 ffmpeg_options = {
     'options' : '-vn'
 }
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume= 0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ''
+        
+    @classmethod
+    async def from_url(cls, url, *, loop= None, stream= None):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+    
