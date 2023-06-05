fc4358abc593cbe922b4131bd6a9d404814065ea
Hance08 <hancechin08@gmail.com>
2023-06-05 23:53:02 +0800

    Update bot.py
    add comments explaining

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-import discord
-import asyncio
-import os
-from discord.ext import commands
+import discord #匯入模組
+import asyncio #匯入模組
+import os #匯入模組
+from discord.ext import commands #匯入模組
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix= '/' , intents= intents)
+bot = commands.Bot(command_prefix= '/' , intents= intents) # 創建一個 Bot 實例，使用 '/' 作為指令前綴，並啟用所有意圖（Intents）
 
 @bot.event
 async def on_ready():
@@ -13,14 +13,14 @@ async def on_ready():
     print(f'{len(slash)} slash commands can be used !')
 
 async def load_extensions():
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
-            await bot.load_extension(f"cogs.{filename[:-3]}")                
+    for filename in os.listdir("./cogs"):  # 遍歷 "./cogs" 目錄下的所有檔案
+        if filename.endswith(".py"): # 如果檔案結尾是 ".py"
+            await bot.load_extension(f"cogs.{filename[:-3]}") # 載入該擴展（extension）              
 
 
 async def main():
-    async with bot:
-        await load_extensions()
-        await bot.start('MTExMjU0MTg5MDU1OTA5ODk3MA.Gt8LpT.dawktOaj2BmeTI88o-olDHnKWS0RT4i70Rb6RY')
+    async with bot:  # 使用 Bot 實例建立異步上下文
+        await load_extensions() # 載入擴展（extension）
+        await bot.start('MTExMjU0MTg5MDU1OTA5ODk3MA.Gt8LpT.dawktOaj2BmeTI88o-olDHnKWS0RT4i70Rb6RY') # 開始 Bot 的運行
 
-asyncio.run(main())
+asyncio.run(main()) # 執行 main 函式的異步運行
